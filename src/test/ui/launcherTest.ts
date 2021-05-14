a0ffad0690a32bae1e3eba0d0494014222743e24
amakarev <alexey.makarevich@intel.com>
2021-05-14 16:25:03 +0300

    Added tests for generating tasks and launch cfg

--- src/test/ui/launcherTest.ts
+++ src/test/ui/launcherTest.ts
@@ -1,8 +1,8 @@
-import { Workbench, VSBrowser } from "vscode-extension-tester";
+import { Workbench, VSBrowser, InputBox, WebDriver, Notification, NotificationType, ModalDialog } from "vscode-extension-tester";
 import { DialogHandler } from "vscode-extension-tester-native";
 import { execSync } from "child_process";
 import { expect } from "chai";
-import { mkdirSync, rmdirSync } from "fs";
+import { copyFileSync, existsSync, mkdirSync, rmdirSync } from "fs";
 import * as path from "path";
 
 describe("Launcher Extension basic tests", () => {
@@ -13,13 +13,12 @@ describe("Launcher Extension basic tests", () => {
 
     before(() => {
         mkdirSync(workspacePath, { recursive: true });
-        
         const sourcePath = path.join(process.cwd(), "src", "test", "ui", "assets", "hello-world.c");
         executablePath = path.join(workspacePath, "hello-world");
         execSync(`gcc ${sourcePath} -o ${executablePath}`);
     });
 
-    before(async function() {
+    before(async function () {
         this.timeout(20000);
 
         workbench = new Workbench();
@@ -32,8 +31,8 @@ describe("Launcher Extension basic tests", () => {
         await browser.driver.sleep(1000);
     });
 
-    it("VTune should run", async function() {
-        this.timeout(30000);  
+    it("VTune should run", async function () {
+        this.timeout(30000);
 
         await workbench.executeCommand("vtune");
         const dialog = await DialogHandler.getOpenDialog();
@@ -47,8 +46,8 @@ describe("Launcher Extension basic tests", () => {
         execSync("killall -9 vtune-gui");
     });
 
-    it("Advisor should run", async function() {
-        this.timeout(30000);  
+    it("Advisor should run", async function () {
+        this.timeout(30000);
 
         await browser.driver.sleep(3000);
         await workbench.executeCommand("advisor");
@@ -66,3 +65,139 @@ describe("Launcher Extension basic tests", () => {
         rmdirSync(workspacePath, { recursive: true });
     });
 });
+
+describe("Generating tasks and launch configuration", async function () {
+    const samplePath = path.join(process.cwd(), "test-data", "sample");
+    const vscodeConfigsPath = path.join(samplePath, '.vscode');
+    const makefilePath = path.join(process.cwd(), "src", "test", "ui", "assets", "Makefile");
+    let driver: WebDriver;
+
+    before(async function () {
+        this.timeout(20000);
+        mkdirSync(samplePath, { recursive: true });
+        copyFileSync(makefilePath, path.join(samplePath, "Makefile"));
+        driver = VSBrowser.instance.driver;
+    });
+
+    describe("Intel oneAPI: Generate tasks", async function () {
+        before(async function () {
+            this.timeout(20000);
+            const workbench = new Workbench();
+            await workbench.executeCommand('File: Open Folder');
+            const dialog = await DialogHandler.getOpenDialog();
+            await dialog.selectPath(samplePath);
+            await dialog.confirm();
+        });
+
+        it('Quick pick contain command', async function () {
+            this.timeout(10000);
+            const workbench = new Workbench();
+            const input = await workbench.openCommandPrompt() as InputBox;
+            await input.setText('>Intel oneAPI: Generate tasks');
+            const pick = await input.findQuickPick('Intel oneAPI: Generate tasks');
+            expect(pick).not.undefined;
+        });
+
+        it('Quick pick contain \'build_dpcpp\' task after executing \'Generate tasks\' command', async function () {
+            this.timeout(10000);
+            const workbench = new Workbench();
+            const input = await workbench.openCommandPrompt() as InputBox;
+            await input.setText('>Intel oneAPI: Generate tasks');
+            await input.selectQuickPick('Intel oneAPI: Generate tasks');
+            await driver.sleep(1000);
+
+            const pick = await input.findQuickPick('build_dpcpp');
+            await driver.sleep(1000);
+            expect(pick).not.undefined;
+        });
+
+        it('Adding the task shows a notification with the correct text', async function () {
+            this.timeout(10000);
+            const workbench = new Workbench();
+            const input = await workbench.openCommandPrompt() as InputBox;
+            await input.setText('>Intel oneAPI: Generate tasks');
+            await input.selectQuickPick('Intel oneAPI: Generate tasks');
+            await driver.sleep(1000);
+            await input.selectQuickPick('build_dpcpp');
+            await driver.sleep(1000);
+            const pick = await input.findQuickPick('buid_dpcpp');
+            const notification = await driver.wait(async () => { return await getNotifications('Task for "build_dpcpp" was added'); }, 10000) as Notification;
+            expect(await notification.getType()).equals(NotificationType.Info);
+        });
+
+        it('.vscode folder contains tasks.json file', function () {
+            const task = path.join(vscodeConfigsPath, 'tasks.json');
+            expect(existsSync(task)).equals(true);
+        });
+    });
+
+    describe('Intel oneAPI: Generate launch configurations', function () {
+        
+        it('Quick pick contain command', async function () {
+            this.timeout(10000);
+            let workbench = new Workbench();
+            let input = await workbench.openCommandPrompt() as InputBox;
+            await input.setText('>Intel oneAPI: Generate launch configurations');
+            const pick = await input.findQuickPick('Intel oneAPI: Generate launch configurations');
+            expect(pick).not.undefined;
+        });
+
+        it('Quick pick contain fake executable', async function () {
+            this.timeout(10000);
+            let workbench = new Workbench();
+            await workbench.executeCommand('Intel oneAPI: Generate launch configurations');
+            await driver.sleep(1000);
+            let input = new InputBox();
+            let pick = await input.findQuickPick('Put temporal target path "a.out" to replace it later with correct path manually');
+            await input.cancel();
+
+            // close warning about debugging
+            const dialog = new ModalDialog();
+            await dialog.pushButton('OK');
+
+            expect(pick).not.undefined;
+        });
+
+        it('Command shows a notification with the correct text', async function () {
+            this.timeout(10000);
+            let workbench = new Workbench();
+            workbench.executeCommand('Intel oneAPI: Generate launch configurations');
+            await driver.sleep(1000);
+            let input = new InputBox();
+            await input.selectQuickPick('Put temporal target path "a.out" to replace it later with correct path manually');
+
+            // close note about debugging launch template
+            const dialog = new ModalDialog();
+            await dialog.pushButton('OK');
+
+            await input.cancel();
+            await input.cancel();
+            await input.cancel();
+
+            // close debug warning on non-CPU devices
+            const debugWarning = new ModalDialog();
+            await debugWarning.pushButton('OK');
+
+            const notification = await driver.wait(async () => { return await getNotifications('Launch configuration "Launch_template" for "a.out" was added'); }, 10000) as Notification;
+            expect(await notification.getType()).equals(NotificationType.Info);
+        });
+
+        it('.vscode folder contains launch.json file', function () {
+            this.timeout(10000);
+            const launch = path.join(vscodeConfigsPath, 'launch.json');
+            expect(existsSync(launch)).equals(true);
+        });
+    });
+    after(() => {
+        rmdirSync(samplePath, { recursive: true });
+    });
+});
+async function getNotifications(text: string): Promise<Notification | undefined> {
+    const notifications = await new Workbench().getNotifications();
+    for (const notification of notifications) {
+        const message = await notification.getMessage();
+        if (message.indexOf(text) >= 0) {
+            return notification;
+        }
+    }
+}

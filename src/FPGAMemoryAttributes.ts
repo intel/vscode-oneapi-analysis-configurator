1bbe8881f507682ea19a6ce33838e9c6f8b3c47c
akurkina <anastasiia.kurkina@intel.com>
2021-08-24 17:42:37 +0300

    fix: add signature

--- src/FPGAMemoryAttributes.ts
+++ src/FPGAMemoryAttributes.ts
@@ -11,38 +11,38 @@ export const attributes: IAttributes = {
   },
   numbanks: {
     description: 'Specifies that the memory implementing the variable or array must have N memory banks.',
-    signature: 'numbanks(N)'
+    signature: 'intel::numbanks(N)'
   },
   bankwidth: {
     description: 'Specifies that the memory implementing the variable or array must be W bytes wide.',
-    signature: 'bankwidth(W)'
+    signature: 'intel::bankwidth(W)'
   },
   singlepump: {
     description: 'Specifies that the memory implementing the variable or array should be clocked at the same rate as the accesses to it.',
-    signature: 'singlepump'
+    signature: 'intel::singlepump'
   },
   doublepump: {
     description: 'Specifies that the memory implementing the variable or array should be clocked at twice the rate as the accesses to it.',
-    signature: 'doublepump'
+    signature: 'intel::doublepump'
   },
   max_replicates: {
     description: 'Specifies that a maximum of N replicates should be created to enable simultaneous reads from the datapath.',
-    signature: 'max_replicates(N)'
+    signature: 'intel::max_replicates(N)'
   },
   private_copies: {
     description: 'Specifies that a maximum of N private copies should be created to enable concurrent execution of N pipelined threads.',
-    signature: 'private_copies(N)'
+    signature: 'intel::private_copies(N)'
   },
   simple_dual_port: {
     description: 'Specifies that the memory implementing the variable or array should have no port that services both reads and writes.',
-    signature: 'simple_dual_port'
+    signature: 'intel::simple_dual_port'
   },
   merge: {
     description: 'Merge two or more variables or arrays in the same scope width-wise or depth-wise. All variables with the same key string are merged into the same memory system. The string type can be either width or depth.',
-    signature: 'merge("key", "type")'
+    signature: 'intel::merge("key", "type")'
   },
   bank_bits: {
     description: 'Specifies that the local memory addresses should use bits (b0, b1,..., bn) for bank-selection, where (b0, b1,..., bn) are indicated in terms of word-addressing. The bits of the local memory address not included in (b0, b1,..., bn) will be used for word-selection in each bank.',
-    signature: 'bank_bits(b0, b1,..., bn)'
+    signature: 'intel::bank_bits(b0, b1,..., bn)'
   }
 };
